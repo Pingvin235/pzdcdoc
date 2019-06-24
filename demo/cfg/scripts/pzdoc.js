e93597b66066d5d2c81380449ed3ec9688c18191
Shamil Vakhitov <shamil@pzdcdoc.org>
2019-06-24 21:53:52 +0200

    Working pointing to subsections.

--- demo/cfg/scripts/pzdoc.js
+++ demo/cfg/scripts/pzdoc.js
@@ -4,15 +4,33 @@
 const $$ = new function() {
 	const scrollTocToVisible = () => {
 		const toc2 = document.querySelector('#toc.toc2');
-		const selected = toc2.querySelector('li > p > a.current');
+		const selected = $(toc2).find('li a.current').last()[0];
 		toc2.scrollTop = selected.offsetTop - toc2.offsetTop;
+	};
+	
+	const markPart = () => {
+		const mark = function () {
+			const hash = location.hash;
+			if (hash) {
+				const $selected = $('#toc.toc2 li > p > a.current');
+				const $partLinks = $selected.closest('li').find('ul a');
+				
+				$partLinks.removeClass('current');
+				$partLinks.filter('[href="' + hash + '"]').addClass('current');
+			}
+		};
+		
+		mark();
+		$(window).bind('hashchange', mark);
 	};
 	
 	// public functions
 	this.scrollTocToVisible = scrollTocToVisible;
+	this.markPart = markPart;
 }
 
 
 $(function () {
+	$$.markPart();
 	$$.scrollTocToVisible();
 });
