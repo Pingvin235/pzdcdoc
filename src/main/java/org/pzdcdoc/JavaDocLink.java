4d8a4d3c9799b6fa433254a6c10dd9441ccd0bf0
Shamil Vakhitov <shamil@pzdcdoc.org>
2019-09-25 21:55:34 +0200

    Fix javadoc.

--- src/main/java/org/pzdcdoc/JavaDocLink.java
+++ src/main/java/org/pzdcdoc/JavaDocLink.java
@@ -5,22 +5,20 @@ import java.util.Map;
 
 import org.asciidoctor.ast.ContentNode;
 import org.asciidoctor.extension.InlineMacroProcessor;
+import org.asciidoctor.extension.Name;
 
+@Name("javadoc")
 public class JavaDocLink extends InlineMacroProcessor {
     
     // TODO: Make configurable!
     private final String docUrlPrefix = "http://www.bgcrm.ru/doc/3.0/javadoc/";
-
-    JavaDocLink() {
-        super("javadoc");
-    }
 
     @Override
     public Object process(ContentNode parent, String target, Map<String, Object> attributes) {
         Map<String, Object> options = new HashMap<>(2);
         options.put("type", ":link");
         options.put("target", docUrlPrefix + target.replace('.', '/') + ".html");
-        return createPhraseNode(parent, "anchor", target, attributes, options).convert();
+        return createPhraseNode(parent, "anchor", target, attributes, options);
     }
 
 }
