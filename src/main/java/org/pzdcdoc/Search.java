82d6683810552d99c9302646cd680a5a0dd7d67e
Shamil Vakhitov <shamil@pzdcdoc.org>
2019-09-28 22:20:07 +0200

    Write documents.

--- src/main/java/org/pzdcdoc/Search.java
+++ src/main/java/org/pzdcdoc/Search.java
@@ -5,6 +5,11 @@ import java.io.FileOutputStream;
 import java.io.OutputStreamWriter;
 import java.io.Writer;
 import java.nio.charset.StandardCharsets;
+import java.util.ArrayList;
+import java.util.List;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.ObjectWriter;
 
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
@@ -14,17 +19,44 @@ public class Search {
 
     static final String SCRIPT = "pzsearch.js";
 
+    private List<Article> articles = new ArrayList<>();
     
-    public void addArticle(String content) {
-
+    public void addArticle(Article article) {
+        articles.add(article);
     }
 
     public void writeScript(File rootRes) {
         log.info("Write search script.");
         try (Writer out = new OutputStreamWriter(new FileOutputStream(rootRes.getAbsolutePath() + "/" + SCRIPT), StandardCharsets.UTF_8)) {
-            out.write("$$.documents = []");
+            out.write("$$.documents = ");
+            out.write(new ObjectMapper().writeValueAsString(articles));
+            out.write(";");
         } catch (Exception e) {
-            log.error(e);
+            log.error(e.getMessage(), e);
+        }
+    }
+
+    public static class Article {
+        private final String url;
+        private final String title;
+        private final String content;
+
+        public Article(String url, String title, String content) {
+            this.url = url;
+            this.title = title;
+            this.content = content;
+        }
+
+        public String getUrl() {
+            return url;
+        }
+
+        public String getTitle() {
+            return title;
+        }
+
+        public String getContent() {
+            return content;
         }
     }
 }
