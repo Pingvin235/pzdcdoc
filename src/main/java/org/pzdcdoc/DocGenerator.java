4724bf989b411a1941db9fad2b15c90671bbe3bc
Shamil Vakhitov <shamil@pzdcdoc.org>
2021-07-14 13:28:04 +0200

    Warning when using old runner class.

--- src/main/java/org/pzdcdoc/DocGenerator.java
+++ src/main/java/org/pzdcdoc/DocGenerator.java
@@ -1,5 +1,7 @@
 package org.pzdcdoc;
 
+import org.apache.logging.log4j.Logger;
+
 /**
  * Backward compatible runner class.
  * 
@@ -7,7 +9,11 @@ package org.pzdcdoc;
  */
 @Deprecated
 public class DocGenerator {
+    private static final Logger log = LogManager.getLogger();
+
     public static void main(String[] args) throws Exception {
+        log.warning("Use '{}' main class instead", Generator.class.getName());
         Generator.main(new String[] { "--in", args[1], "--out", args[2] });
+        log.warning("Use '{}' main class instead", Generator.class.getName());
     }
 }
