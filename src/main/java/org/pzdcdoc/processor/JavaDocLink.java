0f2037a3719768bef7d16d0494efbfc250042b23
Shamil Vakhitov <shamil@pzdcdoc.org>
2024-04-20 12:30:20 +0200

    Restored old JavaDoc attribute name

--- src/main/java/org/pzdcdoc/processor/JavaDocLink.java
+++ src/main/java/org/pzdcdoc/processor/JavaDocLink.java
@@ -14,23 +14,32 @@ import org.asciidoctor.extension.Name;
  */
 @Name("javadoc")
 public class JavaDocLink extends InlineMacroProcessor {
-    /** JavaDoc root URL attribute name */
-    private static final String ATTR_ROOT_URL = "pzdc-javadoc-root-url";
-    /** JavaDoc root relative path */
-    private static final String ATTR_ROOT_PATH = "pzdc-javadoc-root-path";
+    /** JavaDoc URL path prefix, can be absolute or related to the target root directory */
+    private static final String ATTR_PATH_PREFIX = "pzdc-javadoc";
 
     @Override
     public Object process(ContentNode parent, String target, Map<String, Object> attributes) {
-        String urlPrefix = (String) parent.getDocument().getAttribute(ATTR_ROOT_URL);
-        if (urlPrefix != null) {
-            // the map must be modifiable
-            Map<String, Object> options = new HashMap<>();
-            options.put("type", ":link");
-            options.put("target", urlPrefix + target.replace('.', '/') + ".html");
-            return createPhraseNode(parent, "anchor", target, attributes, options);
+        String urlPrefix = (String) parent.getDocument().getAttribute(ATTR_PATH_PREFIX);
+        if (urlPrefix == null)
+            throw new UnsupportedOperationException("No proper configuration defined for javadoc macros");
+
+        // the map must be modifiable
+        Map<String, Object> options = new HashMap<>();
+        options.put("type", ":link");
+
+        String path = target.replace('.', '/') + ".html";
+
+        String targetOption = null;
+        // absolute path prefix
+        if (urlPrefix.contains("://"))
+            targetOption = urlPrefix + path;
+        // relative
+        else {
+
         }
 
-        throw new UnsupportedOperationException("No proper configuration defined for javadoc macros");
-    }
+        options.put("target", targetOption);
 
+        return createPhraseNode(parent, "anchor", target, attributes, options);
+    }
 }
