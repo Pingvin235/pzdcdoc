a36e534a92b0bcd236ef99e73ae634905982f5cb
Shamil Vakhitov <shamil@pzdcdoc.org>
2024-04-20 18:48:17 +0200

    p14247 Passing command line attributes to Generator. Site title.

--- src/main/java/org/pzdcdoc/Generator.java
+++ src/main/java/org/pzdcdoc/Generator.java
@@ -45,15 +45,18 @@ import org.pzdcdoc.processor.snippet.Snippet;
 public class Generator {
     private static final Logger log = LogManager.getLogger();
 
+    public static final String ATTR_GENERATOR = "pzdc-generator";
+    public static final String ATTR_SOURCE = "pzdc-source";
+    public static final String ATTR_TARGET = "pzdc-target";
+    public static final String ATTR_PATH_TO_ROOT = "pzdc-path-to-root";
+
     private static final String DIR_RES = "_res";
 
     private static final String EXT_ADOC = ".adoc";
     private static final String EXT_ADOCF = ".adocf";
     private static final String EXT_HTML = ".html";
 
-    public static final String ATTR_GENERATOR = "generator";
-    public static final String ATTR_TARGET = "target";
-    public static final String ATTR_SOURCE = "source";
+    private static final String ATTR_SITE_TITLE = "pzdc-site-title";
 
     private final Asciidoctor asciidoctor = Factory.create();
 
@@ -69,10 +72,34 @@ public class Generator {
     private static final String[] STYLESHEETS = { ASCIIDOCTOR_DEFAULT_CSS, PZDCDOC_CSS, FONT_CSS, "coderay-asciidoctor.css" };
     private static final String[] STYLESHEETS_INJECT = { PZDCDOC_CSS, FONT_CSS };
 
+    public static void main(String[] args) throws Exception {
+        var gen = new Generator();
+        var parser = new CmdLineParser(gen);
+        try {
+            parser.parseArgument(args);
+        } catch (CmdLineException e) {
+            System.err.println(e.getMessage());
+            parser.printUsage(System.err);
+            System.exit(1);
+        }
+
+        int errors = gen.process();
+        errors += gen.check();
+
+        log.info("DONE!");
+
+        if (errors > 0) {
+            log.error("ERRORS => {}", errors);
+            System.exit(errors);
+        }
+    }
+
     @Option(required = true, name = "-i", aliases = { "--in" }, usage = "Source directory path")
     private File sourceDir;
     @Option(required = true, name = "-o", aliases = { "--out" }, usage = "Target directory path")
     private File targetDir;
+    @Option(required = false, name = "-a", aliases = { "--attribute" }, usage = "Attribute name=value")
+    private Map<String, String> attributes;
 
     /** Cached ToC from index.adoc for injecting everywhere. */
     private Element toc;
@@ -107,14 +134,14 @@ public class Generator {
         copyScriptsAndStyles();
         deleteTmpFiles();
         if (errors > 0)
-            log.error("PROC ERRORS => " + errors);
+            log.error("PROC ERRORS => {}", errors);
         return errors;
     }
 
     private int check() throws Exception {
         int errors = new Links().checkDir(targetDir);
         if (errors > 0)
-            log.error("CHECK ERRORS => " + errors);
+            log.error("CHECK ERRORS => {}", errors);
         return errors;
     }
 
@@ -148,12 +175,13 @@ public class Generator {
             attributes = loadAttributes(source, attributes);
 
             for (File file : files)
-                process(file, new File(target.getPath() + "/" + file.getName()), depth + 1, attributes);
+                process(file, new File(target, file.getName()), depth + 1, attributes);
         } else {
             if (sourceName.endsWith(EXT_ADOC)) {
-                log.info("Processing: " + source);
+                log.info("Processing: {}", source);
 
                 Path targetPath = Paths.get(target.getPath().replace(EXT_ADOC, EXT_HTML));
+                String pathToRoot = StringUtils.repeat("../", depth);
 
                 var attrs = Attributes.builder()
                     .backend("html5")
@@ -171,19 +199,21 @@ public class Generator {
                 attrs.setAttribute(ATTR_SOURCE, source);
                 attrs.setAttribute(ATTR_TARGET, targetPath);
                 attrs.setAttribute(ATTR_GENERATOR, this);
+                attrs.setAttribute(ATTR_PATH_TO_ROOT, pathToRoot);
 
                 attrs.setAttributes(attributes);
 
                 var options = Options.builder()
                     .toFile(false)
-                    .headerFooter(true)
                     .safe(SafeMode.UNSAFE)
                     .attributes(attrs)
                     .build();
+                options.setHeaderFooter(true);
 
                 String html = asciidoctor.convertFile(source, options);
 
-                html = correctHtmlAndCopyResources(source.toPath(), html, targetPath, depth, new SourceLink(attributes));
+                if (toc != null || !extractToC(html, targetPath))
+                    html = correctHtmlAndCopyResources(source.toPath(), html, targetPath, pathToRoot, new SourceLink(attributes));
 
                 FileUtils.forceMkdirParent(target);
 
@@ -207,6 +237,9 @@ public class Generator {
             for (Node attr : document.selectNodes("//attributes/*"))
                 attributes.put(attr.getName(), attr.getText());
 
+            if (this.attributes != null)
+                attributes.putAll(this.attributes);
+
             log.info("Read {} attributes", attributes.size());
         }
         return attributes;
@@ -215,18 +248,16 @@ public class Generator {
     private void copyScriptsAndStyles() throws IOException {
         log.info("Copying scripts and styles.");
 
-        File rootRes = new File(targetDir + "/" + DIR_RES);
+        File rootRes = new File(targetDir, DIR_RES);
         if (!rootRes.exists()) rootRes.mkdirs();
 
         for (String script : SCRIPTS)
-            IOUtils.copy(getClass().getClassLoader().getResourceAsStream("scripts/" + script),
-                    new FileOutputStream(rootRes.getAbsolutePath() + "/" + script));
+            IOUtils.copy(getClass().getClassLoader().getResourceAsStream("scripts/" + script), new FileOutputStream(new File(rootRes, script)));
 
         search.writeScript(rootRes);
 
         for (String style : STYLESHEETS)
-            IOUtils.copy(getClass().getClassLoader().getResourceAsStream("stylesheets/" + style),
-                    new FileOutputStream(rootRes.getAbsolutePath() + "/" + style));
+            IOUtils.copy(getClass().getClassLoader().getResourceAsStream("stylesheets/" + style), new FileOutputStream(new File(rootRes, style)));
     }
 
     private void deleteTmpFiles() throws IOException {
@@ -244,87 +275,58 @@ public class Generator {
             });
     }
 
+    /**
+     * Extracts ToC to #toc from index file placed on the first place of the root directory.
+     * @param html
+     * @param target
+     */
+    private boolean extractToC(String html, Path target) {
+        if (!containsIndex(target.toString())) {
+            log.debug("Not found index file with ToC");
+            return false;
+        }
+
+        toc = Jsoup.parse(html, StandardCharsets.UTF_8.name());
+        toc = toc.select("body").tagName("div").get(0);
+        // remove class="article"
+        toc.clearAttributes();
+        // add search field
+        search.injectField(toc.select("#header"));
+
+        return true;
+    }
+
     private boolean containsIndex(String name) {
         return name.contains("index");
     }
 
-    private String correctHtmlAndCopyResources(Path source, String html, Path target, int depth, SourceLink linkToSource) throws Exception {
-        log.debug("correctHtml targetPath: {}, deep: {}", target, depth);
-
-        if (toc == null) {
-            // the index file must be placed on the top the root directory
-            if (containsIndex(target.toString())) {
-                toc = Jsoup.parse(html, StandardCharsets.UTF_8.name());
-                toc = toc.select("body").tagName("div").get(0);
-                // add search field
-                search.injectField(toc.select("#header"));
-            }
-            return html;
-        }
+    private String correctHtmlAndCopyResources(Path source, String html, Path target, String pathToRoot, SourceLink linkToSource) throws Exception {
+        log.debug("correctHtml targetPath: {}, pathToRoot: {}", target, pathToRoot);
 
         Document jsoup = Jsoup.parse(html);
         Element head = jsoup.selectFirst("head");
+        Element title = head.selectFirst("title");
 
         // add content to search index
         if (search != null) {
             final String relativePath = targetDir.toPath().relativize(target).toString();
-            search.addArticle(new Search.Article(relativePath, head.select("title").text(), jsoup.text()));
+            search.addArticle(new Search.Article(relativePath, title.text(), jsoup.text()));
         }
+
+        // add html title suffix
+        String siteTitle = attributes == null ? null : attributes.get(ATTR_SITE_TITLE);
+        if (siteTitle != null)
+            title.text(title.text() + " | " + siteTitle);
 
         copyResources(jsoup, source, target);
 
-        injectScriptsAndStyles(depth, head);
+        injectScriptsAndStyles(head, pathToRoot);
 
-        correctToC(jsoup, target, depth);
+        correctToC(jsoup, target, pathToRoot);
 
         linkToSource.inject(jsoup, sourceDir.toPath().relativize(source).toString());
 
         return jsoup.toString();
-    }
-
-    private void injectScriptsAndStyles(int depth, Element head) {
-        var pathPrefix = StringUtils.repeat("../", depth) + DIR_RES  + "/";
-        for (String script : SCRIPTS_INJECT)
-            head.append("<script src='" + pathPrefix + script + "'/>");
-        for (String css : STYLESHEETS_INJECT)
-            head.append("<link rel='stylesheet' href='" + pathPrefix + css + "'>");
-    }
-
-    private void correctToC(Document jsoup, Path target, int depth) {
-        // find of the top ToC
-        Element pageToC = jsoup.selectFirst("#toc.toc");
-        if (pageToC != null) {
-            Element ul = pageToC.selectFirst(".sectlevel1").clone();
-            // remove doesn't work properly
-            pageToC.html("");
-            pageToC = ul;
-        }
-
-        // inject left ToC
-        jsoup.selectFirst("body").addClass("toc2");
-        jsoup.select("#toc").before("<div id=\"toc\" class=\"toc2\">" + toc.toString() + "</div>");
-
-        for (Element a : jsoup.select("#toc.toc2 a")) {
-            Link link = new Link(a);
-            if (link.isExternalReference())
-                continue;
-
-            String href = link.get();
-
-            if (target.endsWith(href)) {
-                a.addClass("current");
-                if (pageToC != null)
-                    a.after(pageToC);
-            }
-            link.set(StringUtils.repeat("../", depth) + href);
-            a.attr("title", a.text());
-        }
-
-        // add link to root on title
-        Element title = jsoup.selectFirst("#toc #header h1");
-        if (title != null) {
-            title.html("<a href='" + StringUtils.repeat("../", depth) + "index.html'>" + title.text() + "</a>");
-        }
     }
 
     private void copyResources(Document jsoup, Path source, Path target) throws IOException {
@@ -363,26 +365,51 @@ public class Generator {
         }
     }
 
-    public static void main(String[] args) throws Exception {
-        var gen = new Generator();
-        var parser = new CmdLineParser(gen);
-        try {
-            parser.parseArgument(args);
-        } catch (CmdLineException e) {
-            System.err.println(e.getMessage());
-            parser.printUsage(System.err);
-            System.exit(1);
-        }
-
-        int errors = gen.process();
-        errors += gen.check();
-
-        log.info("DONE!");
-
-        if (errors > 0) {
-            log.error("ERRORS => " + errors);
-            System.exit(errors);
-        }
+    private void injectScriptsAndStyles(Element head, String pathToRoot) {
+        var pathPrefix = pathToRoot + DIR_RES  + "/";
+        for (String script : SCRIPTS_INJECT)
+            head.append("<script src='" + pathPrefix + script + "'/>");
+        for (String css : STYLESHEETS_INJECT)
+            head.append("<link rel='stylesheet' href='" + pathPrefix + css + "'>");
     }
 
+    private void correctToC(Document jsoup, Path target, String pathToRoot) {
+        Element pageToC = jsoup.selectFirst("#toc.toc");
+        if (pageToC == null)
+            return;
+
+        // set class 'toc2' for body to support it
+        jsoup.selectFirst("body").addClass("toc2");
+        // extract page toc's content
+        Element pageToCRootUl = pageToC.selectFirst(".sectlevel1").clone();
+        pageToC
+            // convert page toc to toc2
+            .attr("class", "toc2")
+            // replace toc2's content by toc
+            .html(toc.toString());
+
+        // going through toc links
+        for (Element a : pageToC.select("a")) {
+            Link link = new Link(a);
+            if (link.isExternalReference())
+                continue;
+
+            String href = link.get();
+
+            if (target.endsWith(href)) {
+                a.addClass("current");
+                // injecting page ToC into the global ToC
+                if (pageToCRootUl != null)
+                    a.after(pageToCRootUl);
+            }
+            // correct link URL related to the actual page
+            link.set(pathToRoot + href);
+            a.attr("title", a.text());
+        }
+
+        // add link to root on title
+        Element title = pageToC.selectFirst("#header h1");
+        if (title != null)
+            title.html("<a href='" + pathToRoot + "index.html'>" + title.text() + "</a>");
+    }
 }
